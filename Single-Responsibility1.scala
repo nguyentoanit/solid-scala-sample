ab1512c24d02b3897b80d740504f19d21f85d012
Toan Nguyen <nguyentoandev@gmail.com>
2019-08-14 15:23:56 +0700

    Single Responsibility

---
+++ Single-Responsibility1.scala
@@ -0,0 +1,5 @@
+class BookManager {
+  def add(book: Book)
+  def remove(id: Long)
+  def update(book: Book)
+}
---
+++ Single-Responsibility2.scala
@@ -0,0 +1,11 @@
+class AddingBookManager {
+  def add(book: Book)
+}
+
+class RemovingBookManager {
+  def remove(id: Long) 
+} 
+
+class UpdatingBookManager {
+  def update(book: Book)
+}
